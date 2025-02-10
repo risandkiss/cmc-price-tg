487406dfb58c67d08ee29458dcdb30747ce9cefe
risandkiss <96646666+risandkiss@users.noreply.github.com>
2025-02-11 03:48:03 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+CMC_API_KEY = 'YOUR_CMC_API_KEY' # Твой API Token CoinMarketCap
+TOKEN = "YOUR_BOT_TOKEN" # Токен твоего бота Telegram
+CHAT_ID = "YOUR_CHAT_ID" # ID Твоего чата с тобой в боте telegram
+loss = 2 # Отклонение от цены в процентах после которого будет отправлено сообщение. # По умолчанию стоит 2%
+
+
